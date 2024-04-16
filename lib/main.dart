a79460b7abd9d7cfb15eef45734165436c4727fc
soonhong99 <67502252+soonhong99@users.noreply.github.com>
2024-04-16 10:00:34 +0900

    feat: column, heading

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,44 @@ class App extends StatelessWidget {
     return MaterialApp(
       // 사람들이 우리 앱의 home(widget)에 갔을 때 무엇을 보여줘야 할까?
       // class name constructor
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-          centerTitle: true,
-          title: Text('hello flutter'),),
-        body: Center(
-          child: Text('hello world!'),
+      home: Scaffold(    
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 
+          40),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.
+                end,
+                children: [
+                  Column(
+                    crossAxisAlignment: 
+                    CrossAxisAlignment.end,
+                    children: [
+                      Text('Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text('Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.
+                          withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
